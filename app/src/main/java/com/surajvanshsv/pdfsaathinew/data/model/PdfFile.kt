787ea4f6b03be24c8b5577afbee45f3fefaac1b5
surajvansh12 <sk658139@gmail.com>
2026-04-26 16:51:35 +0530

    Add data layer with repository implementation and file scanning

---
+++ app/src/main/java/com/surajvanshsv/pdfsaathinew/data/model/PdfFile.kt
@@ -0,0 +1,7 @@
+package com.surajvanshsv.pdfsaathinew.data.model
+
+data class PdfFile(
+    val name: String,
+    val path: String,
+    val size: Long
+)
---
+++ app/src/main/java/com/surajvanshsv/pdfsaathinew/di/AppModule.kt
@@ -0,0 +1,23 @@
+package com.surajvanshsv.pdfsaathinew.di
+
+import com.surajvanshsv.pdfsaathinew.data.repository.PdfRepositoryImpl
+import com.surajvanshsv.pdfsaathinew.domain.repository.PdfRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Provides
+    @Singleton
+    fun providePdfRepository(): PdfRepository {
+        return PdfRepositoryImpl()
+    }
+
+}
